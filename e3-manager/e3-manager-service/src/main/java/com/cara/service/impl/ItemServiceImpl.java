5772d59567061c9f6864a6345b2429f7fb4660a9
xincun10 <31303402+xincun10@users.noreply.github.com>
2018-08-17 10:42:47 +0800

    ssm整合
    解决整合中出现的binding错误，将项目改造为SOA架构

--- e3-manager/e3-manager-service/src/main/java/com/cara/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/com/cara/service/impl/ItemServiceImpl.java
@@ -23,17 +23,19 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public TbItem getItemById(long itemId) {
-		TbItemExample example = new TbItemExample();
-		Criteria criteria = example.createCriteria();
-		//查询条件
-		criteria.andIdEqualTo(itemId);
-		//执行查询
-		List<TbItem> list = itemMapper.selectByExample(example);
-		if(list!=null && list.size()>0)
-		{
-			return list.get(0);
-		}
-		return null;
+//		TbItemExample example = new TbItemExample();
+//		Criteria criteria = example.createCriteria();
+//		//查询条件
+//		criteria.andIdEqualTo(itemId);
+//		//执行查询
+//		List<TbItem> list = itemMapper.selectByExample(example);
+//		if(list!=null && list.size()>0)
+//		{
+//			return list.get(0);
+//		}
+//		return null;
+		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+		return tbItem;
 	}
 
 }
